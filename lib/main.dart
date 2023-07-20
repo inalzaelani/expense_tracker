d2075d0c193711e59bb568c037842bfd50cddbf4
Muhammad Inal Zaelani <inalzaelani2806@gmail.com>
2023-07-20 11:35:37 +0700

    [FIX] Color Scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:expense_tracker/widget/expenses.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const Expenses() ,
-    )
-  );
+var kColorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(
+    255, 62, 147, 218));
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer
+        )
+        ),
+    home: const Expenses(),
+  ));
 }
