cd9e0bc0e8b5ab078eab7952ed16826a3550175b
Muhammad Inal Zaelani <inalzaelani2806@gmail.com>
2023-07-20 08:21:35 +0700

    [ADD] Card Custom List

---
+++ lib/widget/expenses_list/expense_item.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.calendar_today_rounded),
+                    const SizedBox(width: 8),
+                    Text(expense.date.toString()),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
